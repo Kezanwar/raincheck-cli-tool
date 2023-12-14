291508a485ef4e5bf63d762bc8eb34463dbb7576
Kez Anwar <kezanwar@gmail.com>
2023-12-14 18:01:28 +0000

    added env, apikey and started with main api call logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module raincheck-cli-tool
 
 go 1.21.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	env "github.com/joho/godotenv"
+)
+
+func print_exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+func resp_to_json(response http.Response) string {
+	body, ioReadErr := io.ReadAll(response.Body)
+
+	if ioReadErr != nil {
+		panic(ioReadErr)
+	}
+
+	return string(body)
+}
+
+func set_env() map[string]string {
+	envMap, envLoadErr := env.Read(".env")
+
+	if envLoadErr != nil {
+		panic(envLoadErr)
+	}
+	return envMap
+}
 
 func main() {
 
-	fmt.Println("yo")
+	envMap := set_env()
 
+	API_KEY := envMap["API_KEY"]
+
+	URL := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=London&aqi=no", API_KEY)
+
+	res, httpErr := http.Get(URL)
+
+	if httpErr != nil {
+		print_exit("Weather API not available")
+	}
+
+	if res.StatusCode != 200 {
+		print_exit("Weather API not available")
+	}
+
+	json := resp_to_json(*res)
+
+	println(json)
 }
